7c3e8d617e42b6da278ed7a6de6eb1770653e0bd
Kitus100 <kitus100@example.com>
2026-03-13 17:02:49 -0600

    Cleanup: only web app

--- sql/03_ADSCRIBCION_EXT.sql
+++
@@ -1,2 +0,0 @@
--- Run this in Supabase SQL Editor
-ALTER TABLE empleado_adscripciones ADD COLUMN IF NOT EXISTS es_jefe BOOLEAN DEFAULT FALSE;
--- sql/04_LIMITE_FALTAS.sql
+++
@@ -1,2 +0,0 @@
--- Agregar columna limite_falta_min a la tabla turnos
-ALTER TABLE turnos ADD COLUMN IF NOT EXISTS limite_falta_min INT NOT NULL DEFAULT 60;
